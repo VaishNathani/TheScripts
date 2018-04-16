188258e7fc98ba14ed2bf0f6912f5bf9643c7086
VaishNathani <nathanivaishali@gmail.com>
2018-04-16 10:48:53 +0530

    tooltip compo. changed

--- src/app/tooltips/tooltips.component.ts
+++ src/app/tooltips/tooltips.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tooltips.component.css']
 })
 export class TooltipsComponent implements OnInit {
-
+  pageTitle="Examples of Tooltips";
   constructor() { }
 
   ngOnInit() {
